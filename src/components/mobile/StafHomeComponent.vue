d0aa725f2062043e23864fd429dcb4568c8fc18e
Alril <alril.ambanga@gmail.com>
2023-02-18 20:03:34 +0800

    change layout from typescript to javascript

---
+++ src/components/mobile/StafHomeComponent.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="row justify-around">
+    <q-card class="text-center" style="width: 110px">
+      <q-avatar
+        text-color="grey-8"
+        icon="directions"
+        class="q-ma-none q-pa-none"
+      />
+      <q-card-section class="text-center q-pa-none q-mt-sm"
+        >Jemaat</q-card-section
+      >
+    </q-card>
+    <q-card class="text-center" style="width: 110px">
+      <q-avatar
+        text-color="grey-8"
+        icon="directions"
+        class="q-ma-none q-pa-none"
+      />
+      <q-card-section class="text-center q-pa-none q-mt-sm"
+        >Ibadah Raya</q-card-section
+      >
+    </q-card>
+    <q-card class="text-center" style="width: 110px">
+      <q-avatar
+        text-color="grey-8"
+        icon="directions"
+        class="q-ma-none q-pa-none"
+        square
+      />
+      <q-card-section class="text-center q-pa-none q-mt-sm"
+        >Ibadah Tengah Minggu</q-card-section
+      >
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  setup() {
+    return {};
+  },
+};
+</script>
