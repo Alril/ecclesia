2173c45ef27e85a5233f78940d05639b30941804
Alril <alril.ambanga@gmail.com>
2023-02-18 20:42:08 +0800

    update icon

--- src/components/mobile/StafHomeComponent.vue
+++ src/components/mobile/StafHomeComponent.vue
@@ -1,35 +1,19 @@
 <template>
   <div class="row justify-around">
-    <q-card class="text-center" style="width: 110px">
+    <q-card
+      class="text-center"
+      style="width: 110px"
+      v-for="item in fitur"
+      :key="item.title"
+    >
       <q-avatar
         text-color="grey-8"
         icon="directions"
         class="q-ma-none q-pa-none"
       />
-      <q-card-section class="text-center q-pa-none q-mt-sm"
-        >Jemaat</q-card-section
-      >
-    </q-card>
-    <q-card class="text-center" style="width: 110px">
-      <q-avatar
-        text-color="grey-8"
-        icon="directions"
-        class="q-ma-none q-pa-none"
-      />
-      <q-card-section class="text-center q-pa-none q-mt-sm"
-        >Ibadah Raya</q-card-section
-      >
-    </q-card>
-    <q-card class="text-center" style="width: 110px">
-      <q-avatar
-        text-color="grey-8"
-        icon="directions"
-        class="q-ma-none q-pa-none"
-        square
-      />
-      <q-card-section class="text-center q-pa-none q-mt-sm"
-        >Ibadah Tengah Minggu</q-card-section
-      >
+      <q-card-section class="text-center q-pa-none q-mt-sm">{{
+        item.title
+      }}</q-card-section>
     </q-card>
   </div>
 </template>
@@ -38,7 +22,22 @@
 export default {
   // name: 'ComponentName',
   setup() {
-    return {};
+    const fitur = [
+      {
+        title: "Jemaat",
+        link: "",
+      },
+      {
+        title: "Ibadah Raya",
+        link: "",
+      },
+      {
+        title: "Ibadah Tengah Minggu",
+      },
+    ];
+    return {
+      fitur,
+    };
   },
 };
 </script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,11 +1,50 @@
 <template>
-  <q-page padding>
-    <!-- content -->
+  <q-page>
+    <div class="bg-orange-8" style="height: 420px; border-radius: 0 0 7em 7em">
+      <q-toolbar>
+        <q-toolbar-title class="text-center text-white text-bold text-h5"
+          >GBI Ecclesia Manado</q-toolbar-title
+        >
+      </q-toolbar>
+      <q-form class="q-pa-md">
+        <q-input
+          rounded
+          outlined
+          v-model="search"
+          label="Search"
+          bg-color="white"
+        >
+          <template v-slot:prepend>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </q-form>
+      <q-card class="q-mx-lg" style="border-radius: 1.5em">
+        <q-toolbar>
+          <q-toolbar-title class="text-center text-grey-8"
+            >HUT Minggu ini</q-toolbar-title
+          >
+        </q-toolbar>
+        <img src="https://cdn.quasar.dev/img/mountains.jpg" height="150" />
+        <q-card-section>asdfasdfasdf</q-card-section>
+      </q-card>
+    </div>
+    <staf-home class="q-mt-md" />
   </q-page>
 </template>
 
 <script>
+import StafHomeComponent from "src/components/mobile/StafHomeComponent.vue";
+import { ref } from "vue";
 export default {
-  // name: 'PageName',
-}
+  name: "HomePage",
+  components: {
+    StafHome: StafHomeComponent,
+  },
+  setup() {
+    return {
+      search: ref(""),
+    };
+  },
+};
 </script>
